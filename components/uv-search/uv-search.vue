e8711fc40428bbda24e2806695aa9623fbe56d00
wanghg <wanghg95@163.com>
2025-06-11 11:34:04 +0800

    123

--- components/uv-search/uv-search.vue
+++ components/uv-search/uv-search.vue
@@ -181,16 +181,18 @@
 			},
 			// 获取焦点
 			getFocus() {
-				this.focused = true;
 				// 开启右侧搜索按钮展开的动画效果
 				if (this.animation && this.showAction) this.show = true;
 				this.$emit('focus', this.keyword);
+
+        this.$nextTick(() => this.focused = true)
 			},
 			// 失去焦点
 			blur() {
-        this.focused = false;
-
-				this.show = false;
+				// 最开始使用的是监听图标@touchstart事件，自从hx2.8.4后，此方法在微信小程序出错
+				// 这里改为监听点击事件，手点击清除图标时，同时也发生了@blur事件，导致图标消失而无法点击，这里做一个延时
+        this.$nextTick(() => this.focused = false)
+        this.show = false;
 				this.$emit('blur', this.keyword);
 			},
 			// 点击搜索框，只有disabled=true时才发出事件，因为禁止了输入，意味着是想跳转真正的搜索页
