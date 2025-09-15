f22489417128045d747135b40cbe107a1bde990d
wanghg <wanghg95@163.com>
2025-09-15 13:47:31 +0800

    4444

--- components/uv-upload/utils.js
+++ components/uv-upload/utils.js
@@ -73,7 +73,7 @@ export function chooseFile({
     return new Promise((resolve, reject) => {
         switch (accept) {
         case 'image':
-            if (!uni.chooseImage) {
+            if (uni.chooseFile) {
                 uni.chooseFile({
                     count: multiple ? maxCount : 1,
                     type: 'image',
